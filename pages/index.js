80cb8b569dfaa01b1d2c760cfce841ae5a018158
Ali AHMAD <ali_ahmad94@icloud.com>
2025-05-06 23:38:16 +0200

    Début du portfolio

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import '../styles/home.css'
+
+export default function Home(){
+    return(
+        <main>
+            <h1>Bienvenue sur mon portfolio</h1>
+            <p>Je suis ingénieur systèmes numériques - passionné par l'embarqué, le C, Linux et plus encore.</p>
+        </main>
+    )
+}
